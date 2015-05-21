46509cbc7a46a14db71c68bc8e23083400e182c1
tankerkiller125 <tankerkiller125@gmail.com>
2015-05-21 19:50:03 -0400

    Language file almost done just have to test it

--- application/core/Language.php
+++ application/core/Language.php
@@ -8,6 +8,7 @@
 class Language {
 
     private static $file = '.lang.json';
+    private static $userLangQuery = null;
 
     /**
      * Get the site language
@@ -23,7 +24,7 @@ class Language {
      * @return mixed
      */
     public static function getText($textID) {
-        $userLang = self::getUserLanguage();
+        $userLang = self::getUserLanguage(Session::get('user_id'));
         // If the user's language is set to null
         if (!isset($userLang)) {
             $language_file = self::getDefaultLang() . self::$file;
@@ -31,20 +32,31 @@ class Language {
             return $array[$textID];
         } else {
             // If the user's language is set.
-            $language_file = self::getUserLanguage() . self::$file;
+            $language_file = self::getUserLanguage(Session::get('user_id')) . self::$file;
             $array = json_decode($language_file, true);
             return $array[$textID];
         }
     }
 
     /**
-     * Get the users language (if set)
-     * @return bool
+     * Get the user language (if they are logged in)
+     * @param null $userID
+     * @return mixed|null
      */
-    public static function getUserLanguage() {
-        $user_id = Session::get('user_id');
-        // get user language from the users table.
-        return false;
+    public static function getUserLanguage($userID = null) {
+        if ($userID === null) {
+            return null;
+        } else {
+            if (self::$userLangQuery === null) {
+                self::$userLangQuery = DatabaseFactory::getFactory()
+                    ->getConnection()
+                    ->prepare('SELECT user_lang FROM users WHERE user_id = :user_id');
+            }
+            self::$userLangQuery->execute(array(
+                'user_id' => Session::get('user_id')
+            ));
+            return self::$userLangQuery->fetch();
+        }
     }
 
 
